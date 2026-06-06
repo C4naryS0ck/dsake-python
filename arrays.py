513fc2c5ed5d87b68cdab06f7c3e5a74c7530f6d
Abhishree Raj <abhishree03star@gmail.com>
2026-06-06 18:25:16 +0530

    array basic practice questions

--- arrays.py
+++ arrays.py
@@ -1,1 +1,54 @@
-print('hello')
+"""
+# WAP to search an element in the array. - Linear Search
+#num = int(input("enter a number to search: "))
+num = 45
+arr = [12,23,45,65]
+
+found = False
+
+for i in range(len(arr)):
+    # print(arr[i]) 
+    if num == arr[i]:
+        print("number found at index:",i)
+        found = True
+        break
+
+if not found:
+        print("number not found!") 
+"""
+"""
+array = [10,20,30,22,79]
+for i in range(len(array) -1 , 2,-1):
+    print(array[i])
+"""
+"""
+# WAP to insert an element in the array. 
+array = [10,20,30,22,79]
+num = 90
+idx = 2
+array.append(0)
+print(array)
+
+for i in range(len(array) - 1,idx,-1): 
+    array[i] = array[i-1]
+
+array[idx] = num
+print("new array is: ",array) 
+"""
+"""
+# WAP to delete an element from the array 
+array = [10,23,34,56,67,88,99,90]
+idx = 2
+for i in range(idx,len(array)-1): 
+    array[i] = array[i+1]
+array.pop() # it is used to pop the last duplicate element of the array. 
+print("new array is:", array)
+"""
+
+# find average of the elements of the array 
+array = [10,23,34,56,67,88,99,90]
+sum = 0
+for i in range(len(array)):
+    sum = sum + array[i] 
+avg = sum /len(array)
+print("average of array is: ",avg)
